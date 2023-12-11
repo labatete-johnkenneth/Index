89027cd19bb6a96af10a3c18684d7669108e6b0f
labatete-johnkenneth <151600683+labatete-johnkenneth@users.noreply.github.com>
2023-12-11 19:48:49 +0800

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-  document.getElementById(`imahe`).addEventListener(`click`, function() {
-    document.getElementById(`ikot`).innerHTML = `<img id="imahe" class="img-fluid rounded-circle d-block mx-auto ikot" src="https://w.wallhaven.cc/full/7p/wallhaven-7px87e.jpg" alt="Farrow's Picture" style="width: 400px;">`;
-  });
